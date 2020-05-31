e023538693ece93552f6e49b7ed7dea918ed0e5f
Andry <gaxeyat298@chordmi.com>
2020-05-31 19:14:51 +0300

    Updates

---
+++ precache-manifest.2e66f6a71b03d5852f5dd084a6959014.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dcf8f5a73358600cd104d4975248649",
+    "url": "/appDouble/index.html"
+  },
+  {
+    "revision": "240aad8cdd908c950130",
+    "url": "/appDouble/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "5bb199bb7c525124972f",
+    "url": "/appDouble/static/css/main.103fd6a1.chunk.css"
+  },
+  {
+    "revision": "240aad8cdd908c950130",
+    "url": "/appDouble/static/js/2.0c51c895.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/appDouble/static/js/2.0c51c895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bb199bb7c525124972f",
+    "url": "/appDouble/static/js/main.838ef0c3.chunk.js"
+  },
+  {
+    "revision": "86a228be7533516b75f7",
+    "url": "/appDouble/static/js/runtime-main.daa1a7d2.js"
+  },
+  {
+    "revision": "36ec06394b9c159b44e3384ae610d353",
+    "url": "/appDouble/static/media/hGFcfhVyfbs.36ec0639.png"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/appDouble/static/media/persik.4e1ec840.png"
+  }
+]);
